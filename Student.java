c1f23efe59bd02c3d282fbd27ec63bfc133d7178
SUJAL2001 <sujalkumardas2@gmail.com>
2025-05-25 13:00:26 +0530

    features/added new Student class

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student{
+    public static void main(String[] args){
+        System.out.println("Hello Sujal");
+        System.out.println("How are you Sujal?");
+        System.out.println("I am fine, thank you!");
+        System.out.println("I am learning Java.");
+        System.out.println("I am learning Java from YouTube.");
+    }
+}
